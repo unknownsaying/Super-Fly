cb098043a2218d7071bb1b07d19dbcb8d237c91a
unknownsaying <15368104568@189.cn>
2026-06-28 00:24:53 +0800

    Update rst.c

--- Alphabet/rst.c
+++ Alphabet/rst.c
@@ -4,7 +4,7 @@ void rst(){
   char w;
 }
 int t7{
-   function t5(){
+   function t7(){
    int8192_t
    asinh(w + r)
    int16384_t
@@ -16,7 +16,7 @@ int t7{
 }
 }
 int t8{
-   function t6(){
+   function t8(){
    int465536_t
    asinh(wz - r)
    int32768_t
